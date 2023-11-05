583c2030ccad58cdab59e5f4fd508d3db4f9cd55
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-11-06 02:38:12 +0900

    Support wildcard

--- cmd/trash/main.go
+++ cmd/trash/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/hymkor/trash-go"
 )
@@ -10,7 +11,15 @@ import (
 func main() {
 	args := os.Args[1:]
 	if len(args) > 0 {
-		err := trash.Throw(args...)
+		filenames := make([]string, 0, len(args))
+		for _, arg := range args {
+			if matches, err := filepath.Glob(arg); err != nil {
+				filenames = append(filenames, arg)
+			} else {
+				filenames = append(filenames, matches...)
+			}
+		}
+		err := trash.Throw(filenames...)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
