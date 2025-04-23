b1a170ec25dbddbc0994c51b7316b84355e60359
HAYAMA_Kaoru <iyahaya@nifty.com>
2025-04-23 12:25:11 +0900

    (CLI) Separated main logic into mains to simplify error handling in main

--- cmd/trash/main.go
+++ cmd/trash/main.go
@@ -8,21 +8,21 @@ import (
 	"github.com/hymkor/trash-go"
 )
 
+func mains(args []string) error {
+	filenames := make([]string, 0, len(args))
+	for _, arg := range args {
+		if matches, err := filepath.Glob(arg); err != nil {
+			filenames = append(filenames, arg)
+		} else {
+			filenames = append(filenames, matches...)
+		}
+	}
+	return trash.Throw(filenames...)
+}
+
 func main() {
-	args := os.Args[1:]
-	if len(args) > 0 {
-		filenames := make([]string, 0, len(args))
-		for _, arg := range args {
-			if matches, err := filepath.Glob(arg); err != nil {
-				filenames = append(filenames, arg)
-			} else {
-				filenames = append(filenames, matches...)
-			}
-		}
-		err := trash.Throw(filenames...)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			os.Exit(1)
-		}
+	if err := mains(os.Args[1:]); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
 	}
 }
